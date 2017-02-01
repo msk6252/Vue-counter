3c45612665833251df1f8fb1e6da46ac557e7abd
vagrant <vagrant@vue.(none)>
2017-02-01 17:35:13 +0900

    [add] first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <button @click="increment()">{{counter}}</button>
+    <button @click="reset()">reset</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    increment () {
+      this.counter += 1
+    },
+    reset () {
+      this.counter = 0
+    }
+  }
+}
+</script>
